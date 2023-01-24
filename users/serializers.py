9088f134bdb80f183f021cddfea5835a2f3b84a7
kamyaD <domisemak@gmail.com>
2023-01-24 07:44:35 +0300

    logged in user return email and profile pic

--- users/serializers.py
+++ users/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'bio', 'profile_pic', 'city', 'country', 'job_title','availability_status')
+        fields = ('id', 'username', 'first_name', 'last_name', 'bio', 'profile_pic', 'city', 'country', 'job_title','availability_status','email')
         read_only_field = ('username',)
 
 class RegisterSerializer(serializers.ModelSerializer):
